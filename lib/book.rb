ac4c696dde4d94eeba93546e6eeecccae0658575
Ave12304 <averyselover@hotmail.com>
2018-06-21 21:36:24 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,1 +1,48 @@
+class book
+  attr_accessor :author, :page_count, :genre
+  attr_reader :tittle
 
+def initialize(tittle)
+  @tittle = tittle
+end
+  
+def tittle
+  @tittle
+end
+
+def author = (author)  
+  @author = author
+end
+
+end
+
+def author
+  @author
+end
+
+def page_count = (num)    
+  @page_count = num
+end
+
+def page_count
+  @page_count = num
+end
+
+end
+
+def genre = (genre)    
+  @genre = genre
+end
+
+def genre
+  @genre 
+end
+
+end
+
+def turn_page
+  puts "Flipping the page...wow, you read fast!"
+end
+
+end
+    
